468830aa0fd5f0f4cc3e0e137bd35eeee9b11da5
Pedro Felix <pedro@adopteunmec.com>
2023-09-30 12:54:31 +0200

    Loader update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,18 +1,10 @@
-import { useRecoilValue } from 'recoil';
-import { recoilDarkMode } from '../state/atoms';
 import {getImgUrl} from '../helper';
 
 function Loader() {
-    const darkMode = useRecoilValue(recoilDarkMode);
-
     return (
         <section className='flex flex-1 flex-col justify-center'>
             <figure className='w-40 animate-spin-slow'>
-                {darkMode ? (
-                    <img className='h-full' src={getImgUrl('loader-dark.svg')} alt='Chargement en cours' />
-                ) : (
-                    <img className='h-full' src={getImgUrl('loader-light.svg')} alt='Chargement en cours' />
-                )}
+                <img className='h-full' src={getImgUrl('loader-light.svg')} alt='Chargement en cours' />
             </figure>
         </section>
     );
